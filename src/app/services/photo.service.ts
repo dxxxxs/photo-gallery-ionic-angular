b0a522a3d283b02fa224593a0b99f35cfc4aacf3
oscar <oscar.gonper.3@educa.jcyl.es>
2024-06-05 01:30:09 +0200

    Loading Photos from the Filesystem

--- src/app/services/photo.service.ts
+++ src/app/services/photo.service.ts
@@ -18,6 +18,7 @@ export class PhotoService {
   constructor() { }
 
   public photos: UserPhoto[] = [];
+  private PHOTO_STORAGE: string = 'photos';
 
   public async addNewToGallery() {
     // Take a photo
@@ -30,6 +31,11 @@ export class PhotoService {
     // Save the picture and add it to photo collection
     const savedImageFile = await this.savePicture(capturedPhoto);
     this.photos.unshift(savedImageFile);
+
+    Preferences.set({
+      key: this.PHOTO_STORAGE,
+      value: JSON.stringify(this.photos),
+    });
   }
 
   private async savePicture(photo: Photo) {
@@ -68,4 +74,23 @@ export class PhotoService {
     };
     reader.readAsDataURL(blob);
   });
+
+
+  public async loadSaved() {
+    // Retrieve cached photo array data
+    const { value } = await Preferences.get({ key: this.PHOTO_STORAGE });
+    this.photos = (value ? JSON.parse(value) : []) as UserPhoto[];
+
+    // Display the photo by reading into base64 format
+    for (let photo of this.photos) {
+      // Read each saved photo's data from the Filesystem
+      const readFile = await Filesystem.readFile({
+        path: photo.filepath,
+        directory: Directory.Data,
+      });
+
+      // Web platform only: Load the photo as base64 data
+      photo.webviewPath = `data:image/jpeg;base64,${readFile.data}`;
+    }
+  }
 }
